0a4def5e31f3efdfc77c3823fcefe5022a32034a
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-05-08 21:28:31 +0500

    add lang panel

--- src/blocks/main/main.js
+++ src/blocks/main/main.js
@@ -6,7 +6,7 @@ import { throttle } from "../../js/libs/utils";
 
 	if (arrDown && feedBack) {
 		window.addEventListener('scroll', throttle(() => {
-			const inViewport = feedBack.getBoundingClientRect().top - window.innerHeight < 0;
+			const inViewport = feedBack.getBoundingClientRect().top - window.innerHeight < -400;
 			arrDown.classList.toggle('hidden', inViewport);
 		}, 200), { passive: true });
 	}
